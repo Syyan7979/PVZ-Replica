5771bf122dfc24d8a7fb09b75eeff6cca9ff11e8
Kurt Ian Khalid Israel <kiisrael@up.edu.ph>
2022-04-09 18:11:52 +0800

    Added Map Layout, Walking Mechanic with animation, Player Attack mechanics with animation, enemy AI (attack and player follow algorithm).

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    // Config Params
+    [SerializeField] Canvas settings;
+
+    // Global Variable
+    Button[] buttons1;
+    public void Start()
+    {
+        buttons1 = FindObjectsOfType<Button>();
+    }
+    public void Options()
+    {
+        settings.gameObject.SetActive(true);
+        foreach(Button buttons in buttons1)
+        {
+            buttons.enabled = false;
+        }
+    }
+}
